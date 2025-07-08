f4477275a30ff28f9ca98f581ef8846d0b3d41bc
JungHoon Lee <105867850+MeritEnding@users.noreply.github.com>
2025-07-08 23:39:30 +0900

    Delete agent_spring directory

--- agent_spring/agent_spring/src/main/java/com/agent_Spring/agent_spring/answer/AnswerRepository.java
+++
@@ -1,6 +0,0 @@
-package com.agent_Spring.agent_spring.answer;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AnswerRepository extends JpaRepository<Answer, Integer> {
-}
