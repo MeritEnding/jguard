d0015334d4c53598ba8cd5b889d591d7b3b07438
이정훈 <dksldsk@naver.com>
2025-11-05 23:54:20 +0900

    Initialize Spring Boot backend

---
+++ jguard_backend/src/main/java/com/jguard/jguard_backend/JguardBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jguard.jguard_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JguardBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JguardBackendApplication.class, args);
+	}
+
+}
---
+++ jguard_backend/src/test/java/com/jguard/jguard_backend/JguardBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jguard.jguard_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JguardBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
