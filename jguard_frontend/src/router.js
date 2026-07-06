a64cf423564370c35ef70654a1aae756de944100
이정훈 <dksldsk@naver.com>
2026-07-06 16:42:46 +0900

    feat(user-front): 회원가입 폼 화면 구현

--- jguard_frontend/src/router.js
+++ jguard_frontend/src/router.js
@@ -1,11 +1,18 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Home from './Home';
 
+import Signup_form from './Signup_form';
+
 const router = createBrowserRouter([
     {
         id: 0,
         path: '/',
         element: <Home />,
+    },
+    {
+        id:8,
+        path: '/signup',
+        element: <Signup_form/>
     }
 
 ]);
