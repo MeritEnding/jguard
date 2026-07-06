81990832e644e21c9934edfcf6c23bfac629ca27
이정훈 <dksldsk@naver.com>
2026-07-06 16:38:49 +0900

    feat(answer): 답변 등록·수정·삭제 API 구현

---
+++ jguard_backend/src/main/java/com/jguard/jguard_backend/answer/AnswerDto.java
@@ -0,0 +1,9 @@
+package com.jguard.jguard_backend.answer;
+
+import java.time.LocalDateTime;
+
+public class AnswerDto {
+    private Integer id;
+    private String content;
+    private LocalDateTime createDate;
+}
