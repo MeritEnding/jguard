543f3a40e03dad13a435321b4b78efa071867ee2
이정훈 <dksldsk@naver.com>
2026-07-06 17:01:57 +0900

    refactor(user): 회원가입 예외 처리를 전역 핸들러로 위임 (#18)

--- jguard_backend/src/main/java/com/jguard/jguard_backend/user/UserController.java
+++ jguard_backend/src/main/java/com/jguard/jguard_backend/user/UserController.java
@@ -1,16 +1,13 @@
 package com.jguard.jguard_backend.user;
 
-
-import lombok.RequiredArgsConstructor;
-import org.apache.coyote.Response;
-import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.validation.BindingResult;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,31 +18,17 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/signup")
-    public ResponseEntity<?> signup(@Valid @RequestBody UserCreateForm userCreateForm, BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {
-            return ResponseEntity.badRequest().body(bindingResult.getAllErrors());
-        }
-
+    public ResponseEntity<String> signup(@Valid @RequestBody UserCreateForm userCreateForm) {
         if (!userCreateForm.getPassword1().equals(userCreateForm.getPassword2())) {
-            return ResponseEntity.badRequest().body("비밀번호가 일치하지 않습니다.");
+            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
         }
 
-        try {
-            userService.create(
-                    userCreateForm.getUsername(),
-                    userCreateForm.getEmail(),
-                    userCreateForm.getPassword1(),
-                    "ROLE_USER"
-            );
-            return ResponseEntity.ok("회원가입 성공");
-        }
-        catch (DataIntegrityViolationException e){
-            return ResponseEntity
-                    .status(HttpStatus.CONFLICT)
-                    .body("이미 존재하는 사용자입니다.");
-        }
-        catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("회원가입 실패: " + e.getMessage());
-        }
+        userService.create(
+                userCreateForm.getUsername(),
+                userCreateForm.getEmail(),
+                userCreateForm.getPassword1(),
+                "ROLE_USER"
+        );
+        return ResponseEntity.ok("회원가입 성공");
     }
 }
