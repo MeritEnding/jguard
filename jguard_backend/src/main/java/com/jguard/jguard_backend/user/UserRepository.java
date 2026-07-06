917cbb58455c935b6e52405d355730f25fdcd3d2
이정훈 <dksldsk@naver.com>
2026-07-06 16:38:05 +0900

    feat(user): 회원 도메인 모델 및 회원가입 API 구현

---
+++ jguard_backend/src/main/java/com/jguard/jguard_backend/user/UserRepository.java
@@ -0,0 +1,10 @@
+package com.jguard.jguard_backend.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<SiteUser, Long> {
+    Optional<SiteUser> findByUsername(String username);
+
+}
